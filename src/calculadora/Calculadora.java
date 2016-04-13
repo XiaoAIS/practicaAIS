c480173a1eff38f0ca07ea6cd9c7a51b2d5bfb26
XiaoAIS <xiaospanish@yahoo.es>
2016-04-13 10:05:45 +0200

    ssegunda version

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,15 +1,21 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package calculadora;
 
-
+/**
+ *
+ * @author xiaos
+ */
 public class Calculadora {
 
-    public Calculadora() {
-        new Ventana_principal(new Operador());
-    }
-
-    public static void main(String[] args){
-            new  Calculadora();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
     }
     
 }
